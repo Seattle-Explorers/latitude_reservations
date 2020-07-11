dbb324c4a3ab44c40173f4471a46b940d8684d08
Janet Zheng <janet.zhengg@gmail.com>
2020-07-10 18:29:19 -0700

    rewrote seeding for listing table

---
+++ SDC-seed/PostgreSQL/listingCSVMaker.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const writeListings = fs.createWriteStream('listingTable.csv');
+writeListings.write('listingId,reviews,avatar\n', 'utf8');
+
+const randomNum = (min, max) => (Math.floor(Math.random() * (max - min + 1) + min));
+
+function writeTenMillion(writer, encoding, cb) {
+  let i = 10000000;
+  let id = 0;
+
+  function write () {
+    let ok = true;
+
+    do {
+      i -= 1;
+      id += 1;
+      const listingId = id.toString().padStart(7, '0');
+      const reviews = randomNum(50, 300);
+      const price = randomNum(50, 400);
+      const data = `${listingId},${reviews}, ${price}\n`;
+
+      if (i === 0) {
+        writer.write(data, encoding, cb);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+writeTenMillion(writeListings, 'utf-8', () => {
+  writeListings.end();
+});
+
+module.exports = randomNum;
