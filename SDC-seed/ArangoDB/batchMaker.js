f0b1e59f7c350c675899e0b92f0232cc15957f85
Janet Zheng <janet.zhengg@gmail.com>
2020-07-13 17:54:53 -0700

    deleted original json maker. changed batch file script

--- SDC-seed/ArangoDB/batchMaker.js
+++ SDC-seed/ArangoDB/batchMaker.js
@@ -12,12 +12,12 @@ const writeBatches = (fileName, start, stop) => {
 };
 
 writeBatches('./dataFiles/listing0to1M.json', 0, 1000000);
-writeBatches('./dataFiles/listing1Mto2M.json', 1000001, 2000000);
-writeBatches('./dataFiles/listing2Mto3M.json', 2000001, 3000000);
-writeBatches('./dataFiles/listing3Mto4M.json', 3000001, 4000000);
-writeBatches('./dataFiles/listing4Mto5M.json', 4000001, 5000000);
-writeBatches('./dataFiles/listing5Mto6M.json', 5000001, 6000000);
-writeBatches('./dataFiles/listing6Mto7M.json', 6000001, 7000000);
-writeBatches('./dataFiles/listing7Mto8M.json', 7000001, 8000000);
-writeBatches('./dataFiles/listing8Mto9M.json', 8000001, 9000000);
-writeBatches('./dataFiles/listing9Mto10M.json', 9000001, 10000000);
+writeBatches('./dataFiles/listing1Mto2M.json', 1000000, 2000000);
+writeBatches('./dataFiles/listing2Mto3M.json', 2000000, 3000000);
+writeBatches('./dataFiles/listing3Mto4M.json', 3000000, 4000000);
+writeBatches('./dataFiles/listing4Mto5M.json', 4000000, 5000000);
+writeBatches('./dataFiles/listing5Mto6M.json', 5000000, 6000000);
+writeBatches('./dataFiles/listing6Mto7M.json', 6000000, 7000000);
+writeBatches('./dataFiles/listing7Mto8M.json', 7000000, 8000000);
+writeBatches('./dataFiles/listing8Mto9M.json', 8000000, 9000000);
+writeBatches('./dataFiles/listing9Mto10M.json', 9000000, 10000000);
